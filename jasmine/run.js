c700946a7b8afb785574300cea6a4319e83c9802
Júnior Sbrissa <jr.sbrissa@hotmail.com>
2020-11-24 18:35:34 -0300

    Update report tool

--- jasmine/run.js
+++ jasmine/run.js
@@ -2,8 +2,8 @@ import Jasmine from 'jasmine'
 import 'dotenv/config'
 
 const HtmlReporter = require('jasmine-pretty-html-reporter').Reporter
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter
 const MarkdownReporter = require('jasmine-markdown-reporter')
+const JasmineConsoleReporter = require('jasmine-console-reporter')
 const jasmine = new Jasmine()
 
 jasmine.loadConfigFile('jasmine/support/jasmine.json')
@@ -16,10 +16,16 @@ jasmine.addReporter(
 )
 
 jasmine.addReporter(
-  new SpecReporter({
-    spec: {
-      displayPending: true
-    }
+  new JasmineConsoleReporter({
+    colors: 1, // (0|false)|(1|true)|2
+    cleanStack: 1, // (0|false)|(1|true)|2|3
+    verbosity: 4, // (0|false)|1|2|(3|true)|4|Object
+    listStyle: 'indent', // "flat"|"indent"
+    timeUnit: 'ms', // "ms"|"ns"|"s"
+    timeThreshold: { ok: 25000, warn: 30000, ouch: 35000 }, // Object|Number
+    activity: false, // boolean or string ("dots"|"star"|"flip"|"bouncingBar"|...)
+    emoji: false,
+    beep: false
   })
 )
 
@@ -29,5 +35,14 @@ jasmine.addReporter(
     destination: 'jasmine/report/spec.md'
   })
 )
+
+// const SpecReporter = require('jasmine-spec-reporter').SpecReporter
+// jasmine.addReporter(
+//   new SpecReporter({
+//     spec: {
+//       displayPending: true
+//     }
+//   })
+// )
 
 jasmine.execute()
